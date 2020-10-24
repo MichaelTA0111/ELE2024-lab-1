744423e614fdb615a2c561e8e525d1f4bcc8ab0e
MichaelTA <michaelallen0111@gmail.com>
2020-10-24 14:43:34 +0100

    Finished question 1 (without comments)

--- question-1.py
+++ question-1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 class Car:
 
-    def __init__(self, length=2.,
+    def __init__(self, length=2.3,
                  velocity=5.,
                  x_position=0.,
                  y_position=0.,
@@ -16,13 +16,18 @@ class Car:
         self.__y_position = y_position
         self.__pose = pose
 
-    # Step 1 - Define the system dynamics
-    def move(self, steering_angle, dt):
+    def move(self, steering_angle, dt=0):
+        # Step 1 - Define the system dynamics
         def system_dynamics(t, z):
-            # Defines the system dynamics given in equations 2.1a-c
-            #             [ v * cos(theta) ]
-            # g(t, z)   = [ v * sin(theta) ]
-            #             [ v * tan(u) / L ]
+            """
+            Defines the system dynamics given in equations 2.1a-c
+                         [ v * cos(theta) ]
+             g(t, z)   = [ v * sin(theta) ]
+                         [ v * tan(u) / L ]
+            :param t: The variable time, which remains unused in this system of equations
+            :param z: An array containing the x position, y position and pose of the system
+            :return:
+            """
             theta = z[2]
             return [self.__velocity * np.cos(theta),
                     self.__velocity * np.sin(theta),
@@ -35,61 +40,47 @@ class Car:
 
         # Step 3 - Call solve_ivp
         solution = solve_ivp(system_dynamics,
-                             [0, dt],
-                             z_initial)
+                             [0, t_final],
+                             z_initial,
+                             t_eval=np.linspace(0, t_final, num_points))
 
         self.__x_position = solution.y[0][-1]
         self.__y_position = solution.y[1][-1]
         self.__pose = solution.y[2][-1]
-        # print(solution)
 
-    def get_x(self):
-        return self.__x_position
-
-    def get_y(self):
-        return self.__y_position
-
-    def get_pose(self):
-        return self.__pose
+        return solution
 
 
-class PidController:
+car_1_1 = Car(x_position=0, y_position=0.3, pose=-np.pi / 36)
+car_steering_angle = -np.pi / 90
+t_final = 2
+num_points = 1000
+car_trajectory = car_1_1.move(car_steering_angle)
 
-    def __init__(self, kp, kd, ki, ts):
-        self.__kp = kp
-        self.__kd = kd / ts
-        self.__ki = ki * ts
-        self.__ts = ts
-        self.__error_previous = None
-        self.__sum_errors = 0
-
-    def control(self, y, set_point=0.):
-        error = set_point - y
-        u = self.__kp * error
-
-        if self.__error_previous is not None:
-            u += self.__kd * (error - self.__error_previous)
-
-        u += self.__ki * self.__sum_errors
-        self.__error_previous = error
-        self.__sum_errors += error
-
-        return u
-
-
-murphy = Car(y_position=0.5)
-t_sampling = 0.01
-pid = PidController(0.25, 0.08, 0., t_sampling)
-
-y_cache = np.array([murphy.get_y()])
-for t in range(2000):
-    steering_angle = pid.control(murphy.get_y())
-    murphy.move(steering_angle, t_sampling)
-    y_cache = np.vstack((y_cache, [murphy.get_y()]))
-
-t_span = t_sampling * np.arange(2001)
-plt.plot(y_cache)
-plt.grid()
+# Question 1.1 - x position (m) against time (s)
+plt.plot(car_trajectory.t, car_trajectory.y[0].T)
 plt.xlabel('Time (s)')
-plt.ylabel('y (m)')
+plt.ylabel('x position (m)')
+plt.grid()
+plt.show()
+
+# Question 1.1 - y position (m) against time (s)
+plt.plot(car_trajectory.t, car_trajectory.y[1].T)
+plt.xlabel('Time (s)')
+plt.ylabel('y position (m)')
+plt.grid()
+plt.show()
+
+# Question 1.1 - theta (rad) against time (s)
+plt.plot(car_trajectory.t, car_trajectory.y[2].T)
+plt.xlabel('Time (s)')
+plt.ylabel('theta (rad)')
+plt.grid()
+plt.show()
+
+# Question 1.2 - y position (m) against x position (m)
+plt.plot(car_trajectory.y[0], car_trajectory.y[1].T)
+plt.xlabel('x position (m)')
+plt.ylabel('y position (m)')
+plt.grid()
 plt.show()
