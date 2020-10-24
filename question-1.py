32cad67c3d35b3a524295d0d1a14fe0e5925264d
MichaelTA <michaelallen0111@gmail.com>
2020-10-24 16:32:42 +0100

    Commented question 1

--- question-1.py
+++ question-1.py
@@ -4,19 +4,36 @@ import matplotlib.pyplot as plt
 
 
 class Car:
+    """
+    Class to define the Car object
+    """
 
     def __init__(self, length=2.3,
                  velocity=5.,
                  x_position=0.,
                  y_position=0.,
                  pose=0.):
+        """
+        Constructor for the Car class
+        :param length: The length of the car in metres
+        :param velocity: The velocity of the car in metres per second
+        :param x_position: The x position of the car in metres
+        :param y_position: The y position of the car in metres
+        :param pose: The angle the car makes with the positive x axis in an anticlockwise direction
+        """
         self.__length = length
         self.__velocity = velocity
         self.__x_position = x_position
         self.__y_position = y_position
         self.__pose = pose
 
-    def move(self, steering_angle, dt=0):
+    def move(self, steering_angle, dt=0.001):
+        """
+        Function to make the Car object move according to the dynamics of the system
+        :param steering_angle: The steering angle of the car in radians
+        :param dt: The difference between consecutive time values in seconds
+        :return: The solution describing the system dynamics over time
+        """
         # Step 1 - Define the system dynamics
         def system_dynamics(t, z):
             """
@@ -24,9 +41,9 @@ class Car:
                          [ v * cos(theta) ]
              g(t, z)   = [ v * sin(theta) ]
                          [ v * tan(u) / L ]
-            :param t: The variable time, which remains unused in this system of equations
+            :param t: The variable time, which is unused in this system of equations
             :param z: An array containing the x position, y position and pose of the system
-            :return:
+            :return: The calculated results for each equation inside function g(t, z)
             """
             theta = z[2]
             return [self.__velocity * np.cos(theta),
@@ -40,45 +57,47 @@ class Car:
 
         # Step 3 - Call solve_ivp
         solution = solve_ivp(system_dynamics,
-                             [0, t_final],
+                             [0., t_final],
                              z_initial,
-                             t_eval=np.linspace(0, t_final, num_points))
+                             t_eval=np.linspace(0., t_final, num_points))
 
+        # Step 4 - Store the final values of x, y, and theta in the Car object
         self.__x_position = solution.y[0][-1]
         self.__y_position = solution.y[1][-1]
         self.__pose = solution.y[2][-1]
 
+        # Step 5 - Return the solution describing the system dynamics over time
         return solution
 
+# Declare the Car object and global variables
+car_1 = Car(x_position=0, y_position=0.3, pose=-np.pi / 36.)
+car_steering_angle = np.pi / 90.
+t_final = 2.
+num_points = 1000   # The resolution of the graph
+car_trajectory = car_1.move(car_steering_angle)
 
-car_1_1 = Car(x_position=0, y_position=0.3, pose=-np.pi / 36)
-car_steering_angle = -np.pi / 90
-t_final = 2
-num_points = 1000
-car_trajectory = car_1_1.move(car_steering_angle)
-
-# Question 1.1 - x position (m) against time (s)
+# Question 1.1 - Plot x position (m) against time (s)
 plt.plot(car_trajectory.t, car_trajectory.y[0].T)
 plt.xlabel('Time (s)')
 plt.ylabel('x position (m)')
 plt.grid()
 plt.show()
 
-# Question 1.1 - y position (m) against time (s)
+# Question 1.1 - Plot y position (m) against time (s)
 plt.plot(car_trajectory.t, car_trajectory.y[1].T)
 plt.xlabel('Time (s)')
 plt.ylabel('y position (m)')
 plt.grid()
 plt.show()
 
-# Question 1.1 - theta (rad) against time (s)
+# Question 1.1 - Plot theta (rad) against time (s)
 plt.plot(car_trajectory.t, car_trajectory.y[2].T)
 plt.xlabel('Time (s)')
 plt.ylabel('theta (rad)')
 plt.grid()
 plt.show()
 
-# Question 1.2 - y position (m) against x position (m)
+# Question 1.2 - Plot y position (m) against x position (m)
 plt.plot(car_trajectory.y[0], car_trajectory.y[1].T)
 plt.xlabel('x position (m)')
 plt.ylabel('y position (m)')
