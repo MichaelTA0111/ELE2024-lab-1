9100af4ec2666ed48a6ad1adebf95bef4dd62638
MichaelTA <michaelallen0111@gmail.com>
2020-10-24 22:57:27 +0100

    Made simulating the different cars in a loop

--- question-2-1.py
+++ question-2-1.py
@@ -122,55 +122,37 @@ class PController:
         return u
 
 
+# Declare the global variables
 sampling_rate = 40
 t_final = 50
 t_sampling = 1. / sampling_rate
 ticks = sampling_rate * t_final
 
-car_0 = Car(y_position=0.3, offset_bias=np.deg2rad(1))
-p_0 = PController(0.02, t_sampling)
-x_cache_0 = np.array([car_0.get_x()])
-y_cache_0 = np.array([car_0.get_y()])
-for t in range(ticks):
-    car_steering_angle = p_0.control(car_0.get_y())
-    car_0.move(car_steering_angle, t_sampling)
-    x_cache_0 = np.vstack((x_cache_0, [car_0.get_x()]))
-    y_cache_0 = np.vstack((y_cache_0, [car_0.get_y()]))
+# Declare the arrays to store all of the car objects, x caches, y caches, and P controllers
+car = []
+x_cache = []
+y_cache = []
+p_controller = [PController(0.02, t_sampling),
+                PController(0.1, t_sampling),
+                PController(0.2, t_sampling),
+                PController(0.5, t_sampling)]
 
-car_1 = Car(y_position=0.3, offset_bias=np.deg2rad(1))
-p_1 = PController(0.1, t_sampling)
-x_cache_1 = np.array([car_1.get_x()])
-y_cache_1 = np.array([car_1.get_y()])
-for t in range(ticks):
-    car_steering_angle = p_1.control(car_1.get_y())
-    car_1.move(car_steering_angle, t_sampling)
-    x_cache_1 = np.vstack((x_cache_1, [car_1.get_x()]))
-    y_cache_1 = np.vstack((y_cache_1, [car_1.get_y()]))
+# Simulation of the car with 4 different values of kp
+for i in range(4):
+    car.append(Car(y_position=0.3, offset_bias=np.deg2rad(1)))  # Create a new car to be appended to the car array
+    x_cache.append(np.array([car[i].get_x()]))  # Create a new x cache to be appended to the array of x caches
+    y_cache.append(np.array([car[i].get_y()]))  # Create a new y cache to be appended to the array of y caches
+    for t in range(ticks):
+        car_steering_angle = p_controller[i].control(car[i].get_y())
+        car[i].move(car_steering_angle, t_sampling)
+        x_cache[i] = np.vstack((x_cache[i], [car[i].get_x()]))
+        y_cache[i] = np.vstack((y_cache[i], [car[i].get_y()]))
 
-car_2 = Car(y_position=0.3, offset_bias=np.deg2rad(1))
-p_2 = PController(0.2, t_sampling)
-x_cache_2 = np.array([car_2.get_x()])
-y_cache_2 = np.array([car_2.get_y()])
-for t in range(ticks):
-    car_steering_angle = p_2.control(car_2.get_y())
-    car_2.move(car_steering_angle, t_sampling)
-    x_cache_2 = np.vstack((x_cache_2, [car_2.get_x()]))
-    y_cache_2 = np.vstack((y_cache_2, [car_2.get_y()]))
-
-car_3 = Car(y_position=0.3, offset_bias=np.deg2rad(1))
-p_3 = PController(0.5, t_sampling)
-x_cache_3 = np.array([car_3.get_x()])
-y_cache_3 = np.array([car_3.get_y()])
-for t in range(ticks):
-    car_steering_angle = p_3.control(car_3.get_y())
-    car_3.move(car_steering_angle, t_sampling)
-    x_cache_3 = np.vstack((x_cache_3, [car_3.get_x()]))
-    y_cache_3 = np.vstack((y_cache_3, [car_3.get_y()]))
-
-plt.plot(x_cache_0, y_cache_0, label="K$_p$ = 0.02")
-plt.plot(x_cache_1, y_cache_1, label="K$_p$ = 0.1")
-plt.plot(x_cache_2, y_cache_2, label="K$_p$ = 0.2")
-plt.plot(x_cache_3, y_cache_3, label="K$_p$ = 0.5")
+# Plot all of the car simulations on one graph
+plt.plot(x_cache[0], y_cache[0], label="K$_p$ = 0.02")
+plt.plot(x_cache[1], y_cache[1], label="K$_p$ = 0.1")
+plt.plot(x_cache[2], y_cache[2], label="K$_p$ = 0.2")
+plt.plot(x_cache[3], y_cache[3], label="K$_p$ = 0.5")
 plt.grid()
 plt.xlabel('Time (s)')
 plt.ylabel('y (m)')
